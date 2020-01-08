55818aee19e96f4f75281d0422bb222bf4b08405
barthough <barthough22@gmail.com>
2020-01-08 13:52:57 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ElectionMap from "./components/ElectionMap";
+
+class App extends Component {
+  state = {
+    data: [
+      ["AL", -1],
+      ["AK", 1],
+      ["AZ", 1],
+      ["AR", 1],
+      ["CA", 1],
+      ["CO", 1],
+      ["CT", 1],
+      ["DE", 1],
+      ["DC", 1],
+      ["FL", 1],
+      ["GA", 1],
+      ["HI", 1],
+      ["ID", -1],
+      ["IL", 1],
+      ["IN", -1],
+      ["IA", 1],
+      ["KS", -1],
+      ["KY", 1],
+      ["LA", 1],
+      ["ME", 1],
+      ["MD", 1],
+      ["MA", 1],
+      ["MI", 1],
+      ["MN", 1],
+      ["MS", -1],
+      ["MO", 1],
+      ["MT", -1],
+      ["NE", -1],
+      ["NV", 1],
+      ["NH", 1],
+      ["NJ", 1],
+      ["NM", 1],
+      ["NY", 1],
+      ["NC", 1],
+      ["ND", -1],
+      ["OH", -1],
+      ["OK", -1],
+      ["OR", 1],
+      ["PA", 1],
+      ["RI", 1],
+      ["SC", -1],
+      ["SD", -1],
+      ["TN", -1],
+      ["TX", 1],
+      ["UT", -1],
+      ["VT", 1],
+      ["VA", -1],
+      ["WA", 1],
+      ["WV", 1],
+      ["WI", -1],
+      ["WY", -1]
+    ]
+  };
+  render() {
+    return (
+      <div
+        style={{
+          height: "100vh",
+          width: "100vw"
+        }}
+      >
+        <ElectionMap data={this.state.data} />
+      </div>
+    );
+  }
 }
 
 export default App;
