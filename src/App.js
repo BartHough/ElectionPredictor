1e64ceaebf0287f5cff262277dfe1bbe74308ba6
BartHough <39164910+BartHough@users.noreply.github.com>
2020-01-08 17:48:41 -0700

    Adding api gateway integration

--- src/App.js
+++ src/App.js
@@ -5,61 +5,49 @@ import ElectionMap from "./components/ElectionMap";
 import Description from "./components/Description";
 
 class App extends Component {
-  state = {
-    data: [
-      ["AL", -1],
-      ["AK", 1],
-      ["AZ", 1],
-      ["AR", 1],
-      ["CA", 1],
-      ["CO", 1],
-      ["CT", 1],
-      ["DE", 1],
-      ["DC", 1],
-      ["FL", 1],
-      ["GA", 1],
-      ["HI", 1],
-      ["ID", -1],
-      ["IL", 1],
-      ["IN", -1],
-      ["IA", 1],
-      ["KS", -1],
-      ["KY", 1],
-      ["LA", 1],
-      ["ME", 1],
-      ["MD", 1],
-      ["MA", 1],
-      ["MI", 1],
-      ["MN", 1],
-      ["MS", -1],
-      ["MO", 1],
-      ["MT", -1],
-      ["NE", -1],
-      ["NV", 1],
-      ["NH", 1],
-      ["NJ", 1],
-      ["NM", 1],
-      ["NY", 1],
-      ["NC", 1],
-      ["ND", -1],
-      ["OH", -1],
-      ["OK", -1],
-      ["OR", 1],
-      ["PA", 1],
-      ["RI", 1],
-      ["SC", -1],
-      ["SD", -1],
-      ["TN", -1],
-      ["TX", 1],
-      ["UT", -1],
-      ["VT", 1],
-      ["VA", -1],
-      ["WA", 1],
-      ["WV", 1],
-      ["WI", -1],
-      ["WY", -1]
-    ]
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      dem: '',
+      rep: '',
+      data: []
+
+    };
+  }
+  getData() {
+    fetch(
+      "https://qrc8ez3de6.execute-api.us-east-2.amazonaws.com/Prod/prediction/"
+    )
+      .then(res => res.json())
+      .then(apiData => {
+        this.parseData(apiData);
+      });
+  }
+  parseData(apiData) {
+    const rep = apiData.pop();
+    const dem = apiData.pop();
+    let data = []
+    apiData.forEach(usState => {
+      if(usState[1]< 0) {
+        data.push([usState[0], -1, usState[1]])
+      }
+      else if(usState[1]> 200000) {
+        data.push([usState[0], 1, usState[1]])
+      }
+      else {
+        data.push([usState[0], 0, usState[1]])
+      }
+    });
+    this.setState({
+      ...this.state,
+      rep,
+      dem,
+      data
+    });
+  }
+  componentDidMount() {
+    this.getData();
+  }
   render() {
     return (
       <div
@@ -68,8 +56,8 @@ class App extends Component {
           width: "100%"
         }}
       >
-        <Description />
-        <ElectionMap data={this.state.data} />
+        <Description dem={this.state.dem[1]} rep={this.state.rep[1]}/>
+        {this.state.data.length > 1 && <ElectionMap data={this.state.data} />}
       </div>
     );
   }
--- src/components/Description.js
+++ src/components/Description.js
@@ -1,15 +1,20 @@
-import React, { Component } from 'react'
-import '../styles/Description.css'
-
+import React, { Component } from "react";
+import "../styles/Description.css";
 
 export class Description extends Component {
-    render() {
-        return (
-            <div className='descriptionContainer'>
-                <h2 >2020 Presidential Election Prediction</h2>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div className="descriptionContainer">
+          <h2>2020 Presidential Election Prediction</h2>
+        </div>
+        <div className="votes">
+          <h3>Democratic Electoral Votes: {this.props.dem}</h3>
+          <h3>Republican Electoral Votes: {this.props.rep}</h3>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Description
+export default Description;
